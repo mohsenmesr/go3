5a5734a654dd9222133682325ae5eb02e4492b6f
mohsenmesr <mohsenmesr@yahoo.com>
2021-10-08 22:39:23 +0330

    App configs

--- src/main/java/com/takeaway/go3/config/AsyncConfig.java
+++ src/main/java/com/takeaway/go3/config/AsyncConfig.java
@@ -1,4 +1,9 @@
 package com.takeaway.go3.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnProperty("game.settings.async.brokerAddress")
 public class AsyncConfig {
 }
--- src/main/java/com/takeaway/go3/config/ErrorHandler.java
+++ src/main/java/com/takeaway/go3/config/ErrorHandler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class ExceptionAdvice extends ResponseEntityExceptionHandler {
+public class ErrorHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleException(Exception exception) {
