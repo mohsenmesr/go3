a087f5677d13a6d9ed10ead4df8f2b1f6f79deb7
mohsenmesr <mohsenmesr@yahoo.com>
2021-10-08 22:40:37 +0330

    services

---
+++ src/main/java/com/takeaway/go3/impl/GameServiceImpl.java
@@ -0,0 +1,21 @@
+package com.takeaway.go3.impl;
+
+import com.takeaway.go3.model.Game;
+import com.takeaway.go3.service.GameService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class GameServiceImpl implements GameService {
+
+    @Override
+    public String startGame(int initialValue) {
+        return null;
+    }
+
+    @Override
+    public String play(Game game) {
+        return null;
+    }
+}
--- src/main/java/com/takeaway/go3/service/GameService.java
+++ src/main/java/com/takeaway/go3/service/GameService.java
@@ -1,8 +1,13 @@
 package com.takeaway.go3.service;
 
+import com.sun.istack.internal.NotNull;
 import com.takeaway.go3.model.Game;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.Valid;
+
+@Validated
 public interface GameService {
-    String startGame(Game game);
-    String play(Game game);
+    String startGame(int initialValue);
+    String play(@Valid Game game);
 }
