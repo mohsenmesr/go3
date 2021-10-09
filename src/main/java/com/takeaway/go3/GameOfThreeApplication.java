ea924d3b642e0deb616fc083f551bf6410fda8b3
mohsenmesr <mohsenmesr@yahoo.com>
2021-10-09 17:02:45 +0330

    clean

--- src/main/java/com/takeaway/go3/GameOfThreeApplication.java
+++ src/main/java/com/takeaway/go3/GameOfThreeApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -16,6 +19,14 @@ public class GameOfThreeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GameOfThreeApplication.class, args);
+    }
+
+    @Bean
+    public MessageConverter jacksonJmsMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.BYTES);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
     }
 
     @Bean
--- src/main/java/com/takeaway/go3/service/GameService.java
+++ src/main/java/com/takeaway/go3/service/GameService.java
@@ -1,5 +1,6 @@
 package com.takeaway.go3.service;
 
+import com.takeaway.go3.model.Game;
 import com.takeaway.go3.model.GameStart;
 import org.springframework.validation.annotation.Validated;
 
@@ -7,6 +8,8 @@ import javax.validation.Valid;
 
 @Validated
 public interface GameService {
+
+    String play(Game game);
 
     String startGame(@Valid GameStart gameRequest);
 }
